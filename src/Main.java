fc68633d09ddfe8d77c2e999ae58c41f4f0fb012
AXeonV <2607343351@qq.com>
2025-10-16 17:13:47 +0800

    updated on 16 Oct

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,30 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(), ans = 0;
-		int[] a = new int[n];
-		for (int i = 0; i < n; ++i)
-			a[i] = sc.nextInt();
-		for (int i = 0; i < n; ++i)
-			for (int j = i + 1; j < n; ++j)
-				for (int k = j + 1; k < n; ++k)
-					for (int l = k + 1; l < n; ++l)
-						if (a[i] + a[j] + a[k] + a[l] == 24)
-							++ans;
-		System.out.println(ans);
+		int n = sc.nextInt(), m = sc.nextInt();
+		int[][] arr = new int[n + 2][m + 2];
+		for (int i = 0; i < n + 2; ++i)
+			for (int j = 0; j < m + 2; ++j) {
+				if (i == 0 || j == 0 || i == n + 1 || j == m + 1) arr[i][j] = -1;
+				else arr[i][j] = 0;
+			}
+		int tot = 0, x = 1, y = 1, dx = 0, dy = 1;
+		while (true) {
+			arr[x][y] = sc.nextInt(); ++tot;
+			if (tot == n * m) break;
+			int nx = x + dx, ny = y + dy;
+			if (arr[nx][ny] != 0) {
+				if (dx == 1 && dy == 0) { dx = 0; dy = -1; }
+				else if (dx == -1 && dy == 0) { dx = 0; dy = 1; }
+				else if (dx == 0 && dy == 1) { dx = 1; dy = 0; }
+				else if (dx == 0 && dy == -1) { dx = -1; dy = 0; }
+			}
+			x += dx; y += dy;
+		}
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j)
+				System.out.print(arr[i][j] + " ");
+			System.out.println();
+		}
 	}
 }
